b53a1b942594118447cc3f90dbc48ac8897d0dd1
OJ42 <88906115+OJ42@users.noreply.github.com>
2021-10-06 15:01:07 -0600

    Automation parts 1 and 2

---
+++ Automation Submit/movieList/movieList.test.ts
@@ -0,0 +1,17 @@
+import { Builder, Capabilities, By } from "selenium-webdriver"
+const chromedriver = require('chromedriver')
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+beforeAll(async () => {
+    await driver.get('http://127.0.0.1:5500/movieList/index.html')
+});
+
+afterAll(async () => {
+    await driver.quit()
+});
+
+test('Add Movie', async () => {
+    let form = await driver.findElement(By.id('add'))
+        await form.sendKeys('Interstellar\n')
+        await driver.sleep(5000)
+})
